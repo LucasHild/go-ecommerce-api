403f85e27448592467764a958a14b66e8699c33a
Lucas Hild <contact@lucas-hild.de>
2020-01-11 22:16:46 +0100

    Read environment variables from .env

--- cmd/go-ecommerce-api/main.go
+++ cmd/go-ecommerce-api/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Lanseuo/go-ecommerce-api/pkg/api"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+
 	api.Start()
 }
